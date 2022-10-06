4125ff0df36f37efd40b2b3099c65ac9208980ac
amos-kipngetich <114176862+amos-kipngetich@users.noreply.github.com>
2022-10-06 22:16:52 +0300

    Add files via upload

---
+++ while even loop.cpp
@@ -0,0 +1,13 @@
+//while loop program that executes even numbers.
+#include <iostream>
+using namespace std;
+int main()
+{
+	int count=2;
+	while(count<=50)
+	{
+		cout<<count<<" ";
+		count=count+1+1;
+	}
+	return 0;
+}
