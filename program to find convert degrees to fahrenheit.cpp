2fb7e56d530c01af624cbfff4f7585e93d771183
amos-kipngetich <114176862+amos-kipngetich@users.noreply.github.com>
2022-10-10 12:31:18 +0300

    Add files via upload

---
+++ program to find convert degrees to fahrenheit.cpp
@@ -0,0 +1,14 @@
+//program function to convert degrees to fahrenheit
+#include<iostream>
+using namespace std;
+int main()
+{
+float celcius,fahrenheit;
+cout<<"enter the temperature in celcius:";
+cin>>celcius;
+fahrenheit=((celcius*9)/5)+32;
+cout<<"temperature in fahrenheit is:"<<fahrenheit;
+return(0);
+
+	
+}
